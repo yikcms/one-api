f48491c05b98a185bc1ab4e0d4d80d67c744301f
yikcms <yikcms@126.com>
2023-09-26 17:44:25 +0800

    Update Footer.js

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -28,33 +28,7 @@ const Footer = () => {
   }, []);
 
   return (
-    <Segment vertical>
-      <Container textAlign='center'>
-        {footer ? (
-          <div
-            className='custom-footer'
-            dangerouslySetInnerHTML={{ __html: footer }}
-          ></div>
-        ) : (
-          <div className='custom-footer'>
-            <a
-              href='https://github.com/songquanpeng/one-api'
-              target='_blank'
-            >
-              {systemName} {process.env.REACT_APP_VERSION}{' '}
-            </a>
-            由{' '}
-            <a href='https://github.com/songquanpeng' target='_blank'>
-              JustSong
-            </a>{' '}
-            构建，源代码遵循{' '}
-            <a href='https://opensource.org/licenses/mit-license.php'>
-              MIT 协议
-            </a>
-          </div>
-        )}
-      </Container>
-    </Segment>
+
   );
 };
 
